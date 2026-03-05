87c6acdd6a7be6b3af109003adab94e76491f367
mateus-o-m <mateus.oliveira.macedo@hotmail.com>
2026-03-05 10:16:34 -0300

    Keeping ternary operaor statement as default. Work in progress switch-case structure

--- java-concepts/basics/02-varargsAndEquals.java
+++ java-concepts/basics/02-varargsAndEquals.java
@@ -4,23 +4,27 @@ class usingVarargsAndEquals {
 			System.out.println (numPrint);
 		}
 	}
+
 	public static void main (){
 		System.out.println ("Numbers: ");
 		printNumbers (1, 4, 6, -6, 30);
 
 		String word1 = "casa";
 		String word2 = "Casa";
+
 		String compareResult;
 
-		//compareResult = word1.equals (word2) ? "iguais" : "diferentes";		
+		compareResult = word1.equals (word2) ? "iguais" : "diferentes";		
+		/*em progresso
 		switch (word1.equals (word2)){
-			case (True):
+			case (true):
 				compareResult = "iguais";
 				break;
-			case (False):
+			case (false):
 				compareResult = "diferentes";
 				break;
 		}
+		*/
 
 		System.out.println ("A palavra " + word1 + " e " + word2 + " são " + compareResult);
 	}
