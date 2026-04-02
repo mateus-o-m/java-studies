2e79b250db8542477c50daf3800170fb00787ad6
mateus-o-m <mateus.oliveira.macedo@hotmail.com>
2026-04-02 16:10:10 -0300

    Updating stock.java structure. Adding Product.java

---
+++ concepts/basic/Product.java
@@ -0,0 +1,19 @@
+public class Product {
+   public int quantity;
+   public float price;
+   public int id;
+   public String name;
+
+   public Product (int newQuantity, float newPrice, int newId, String newName) {
+      quantity = newQuantity;
+      price = newPrice;
+      id = newId;
+      name = newName;
+   }
+
+   @Override
+   public String toString() {
+      return "Produto: " + name + ", Quantidade: " + quantity + ", Preço: " + price;
+   }
+}
+
--- concepts/basic/stock.java
+++ concepts/basic/stock.java
@@ -1,22 +1,3 @@
-public class Product {
-   public int quantity;
-   public float price;
-   public int id;
-   public String name;
-
-   public Product (int newQuantity, float newPrice, int newId, String newName) {
-      quantity = newQuantity;
-      price = newPrice;
-      id = newId;
-      name = newName;
-   }
-
-   @Override
-   public String toString() {
-      return "Produto: " + name + ", Quantidade: " + quantity + ", Preço: " + price;
-   }
-}
-
 public class stock {
    public static void main(String[] args) {
       Product banana = new Product(5, 7.8f, 12098, "banana");
