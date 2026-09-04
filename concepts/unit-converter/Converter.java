790c0af2f27eaa466556e65f759f55328e11c726
mateus-o-m <mateus.oliveira.macedo@hotmail.com>
2026-09-04 18:13:26 -0300

    feat: Adding barebone of temperature converter

--- concepts/unit-converter/Converter.java
+++ concepts/unit-converter/Converter.java
@@ -1,3 +1,33 @@
+import java.util.Scanner;
+
 public class Converter {
+    static float celsius;
+    static float fahrenheit;
+    static float kelvin;
+
+    static double celsiusToKelvin(float c) {
+        return (c+273.15);
+    }
+
+    static double celsiusToFahrenheit(float c) {
+        return (c*1.8)+32;
+    }
+
+    static double fahrenheitToCelcius(float f) {
+        return (f-32)/1.8;
+    }
     
+    static double kelvinToCelsius(float k) {
+        return (k-273.15);
+    }
+
+    public static void main() {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Digite a temperatura(°C) que será convertida: ");
+        float valueToConvert = input.nextFloat();
+
+        double convertCelcius = celsiusToFahrenheit(valueToConvert);
+        System.out.print(valueToConvert+"°C é "+convertCelcius +"°F");
+    }
 }
--- concepts/unit-converter/unit-converter.md
+++ concepts/unit-converter/unit-converter.md
@@ -1,1 +1,8 @@
 ## Conversor de Unidades
+
+### Objetivos
+
+Criar calculadora com:
+* Conversor de temperatura (Celsius/Fahrenheit)
+* Conversor de bases decimais (decimal/binário)
+* Realizar cálculos matemáticos (com biblioteca `math`)
